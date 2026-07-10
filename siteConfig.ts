57fb6fe1a8be4cfdcbee6db8eb7f96e5bbb922df
LruiruiU <ba0330719@gmail.com>
2026-07-10 22:04:36 +0800

    Auto sync source code for Vercel 🚀

--- siteConfig.ts
+++ siteConfig.ts
@@ -29,7 +29,7 @@ export const siteConfig = {
 
   // 5. 首页照片墙预览图
   photoWallImage: "https://bu.dusays.com/2026/03/24/69c1e38b4c370.jpg",
-  cloudMusicIds: [],
+  cloudMusicIds: ["637852", "454231889", "418550923", "454231780", "33737123", "418550926", "454231778", "402073", "442562781", "26142299", "402074", "454224831", "2021488376"],
   social: {
     github: "",
     gitee: "",
