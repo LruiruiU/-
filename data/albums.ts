882e7b39bf7c2ed994a98a5ca9290d4f6a738bdf
LruiruiU <ba0330719@gmail.com>
2026-07-11 11:57:07 +0800

    Auto sync source code for Vercel 🚀

--- data/albums.ts
+++ data/albums.ts
@@ -2,4 +2,13 @@
 export interface Photo { url: string; caption?: string; }
 export interface Album { id: string; title: string; description: string; cover: string; date: string; photos: Photo[]; }
 
-export const albums: Album[] = [];
+export const albums: Album[] = [
+  {
+    "title": "啦啦啦~",
+    "description": "新的开始",
+    "cover": "https://bee-reg-ab.imagency.cn/e/f75ef4d5e1db34e0ee4102a04f00141d.png",
+    "id": "album_1783733703360",
+    "photos": [],
+    "date": "2026-07-11"
+  }
+];
